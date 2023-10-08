053b526e2544ca9c844838d074e9a6f9e3aed530
Derek <nagyderek@outlook.com>
2023-10-08 15:56:50 -0400

    here my changes

--- proj1.py
+++ proj1.py
@@ -1,7 +1,16 @@
-name = input("What is you name?")
-age = input("What is you age?")
+import random
+
+name = input("What is your name?")
+age = input("What is your age?")
+car = input("What is your car?")
+cargood = random.randint(1,2)
 
 print(name + age)
+if cargood == 1:
+    print(car + " bad")
+if cargood == 2:
+    print(car + " good")
+
 
 
 
